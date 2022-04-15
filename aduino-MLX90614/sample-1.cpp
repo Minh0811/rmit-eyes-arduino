a552fef2d4a3efa238a5cd53987227b5bc0f2c6d
Minh <vokhaiminh0811@gmail.com>
2022-04-15 22:44:56 +0700

    First sample

---
+++ aduino-MLX90614/sample-1.cpp
@@ -0,0 +1,29 @@
+//Read the temperature value from the sensor every 0.5 seconds
+
+
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+Serial.println("Adafruit MLX90614 test");
+
+mlx.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
+Serial.print("*C\tOject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF());
+Serial.print("F\tOject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+
+Serial.println();
+delay(500);
+}
