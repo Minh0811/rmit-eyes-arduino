ecc8c5d8131fa08026fc2722c581930d3d25f1b6
Minh <vokhaiminh0811@gmail.com>
2022-04-21 22:51:40 +0700

    add images

--- esp8266-MLX90614-HC-SR04/stable-code.cpp
+++ esp8266-MLX90614-HC-SR04/stable-code.cpp
@@ -28,11 +28,9 @@ WiFiClient client;
 //server
 const char* serverName = "http://localhost:3000/";
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
+
 unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
+
 // Set timer to 5 seconds (5000)
 unsigned long timerDelay = 5000;
 
