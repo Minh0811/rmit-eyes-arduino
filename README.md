47667d9d098213e227c84d4132c374cd9ca2d3b0
Minh <vokhaiminh0811@gmail.com>
2022-05-02 23:30:19 +0700

    Added more description to readMe file

--- README.md
+++ README.md
@@ -6,4 +6,35 @@ Components contain: Arduino Uno, MLX90614, HC-SR04, micro servo motor, a physica
 
 ![](images/semi-design.png)
 
+Brief ussage of each components: 
+MLX90614: To capture the temperature of the object; or in this case, body temperature
+HC-SR04(Ultra Sensor): To detect the distance of the object and only record temperature data if only the object is in a certain distance
+Micro servo: This is the automatic door that is control by the button
+Button: If pressed the micro servo will rotate 120 degree and if not, it goes back to 0 degree 
 
+library used: 
+![](images/Libraries.png)
+
+Variables: 
+
+![](images/variables.png)
+
+Setup:
+
+![](images/setup.png)
+
+Motor control Function:
+
+![](images/motorFunction.png)
+
+Calculate object distant with HC-SR04:
+
+![](images/ultraSonic.png)
+
+Return Temperature if the object is in the correct distance:
+
+![](images/printTemp.png)
+
+Loop function:
+
+![](images/loop.png)
--- src/main.cpp
+++ src/main.cpp
@@ -2,100 +2,108 @@
 #include <Servo.h>
 #include <Adafruit_MLX90614.h>
 
-//Motor
-int btn = 4;
+// Motor
+int btn = 4; // button
 int servo = 9;
 int motorAngle = 0;
 int MotorSpeed = 1;
 Servo myservo;
 
-//MLX90614
+// MLX90614
 float temperature, temp;
 float sum;
-int i=0;
+int i = 0;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-//HC-SR04
+// HC-SR04
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+#define trigPin 3 // attach pin D3 Arduino to pin Trig of HC-SR04
+long duration;    // variable for the duration of sound wave travel
+int distance;     // variable for the distance measurement
 #define SOUND_VELOCITY 0.034
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //Motor
+  // Motor
   myservo.attach(servo);
-  myservo.write (0);
+  myservo.write(0);
   pinMode(btn, INPUT_PULLUP);
-  //MLX90614
+  // MLX90614
   mlx.begin();
-  //HC-SR04
+  // HC-SR04
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
 }
 
 void controlMotor()
 {
   int btnState = digitalRead(btn);
-  //Serial.print("State of the button: ");
-  //Serial.println(btnState);
-  //Serial.println(motorAngle);
-  if (btnState == 0)  
+  // Serial.print("State of the button: ");
+  // Serial.println(btnState);
+  // Serial.println(motorAngle);
+  if (btnState == 0)
   {
-    for(int i = 0; i < 120; i++){
-      myservo.write(motorAngle += MotorSpeed); 
-      if(motorAngle > 120)
+    for (int i = 0; i < 120; i++)
+    {
+      myservo.write(motorAngle += MotorSpeed);
+      if (motorAngle > 120)
       {
         motorAngle = 120;
-      }  
-      delay(5);  
+      }
+      delay(5);
     }
   }
   else
   {
-      for(int i = 0; i < 120; i++){
-      myservo.write(motorAngle -= MotorSpeed); 
-      if(motorAngle < 0)
+    for (int i = 0; i < 120; i++)
+    {
+      myservo.write(motorAngle -= MotorSpeed);
+      if (motorAngle < 0)
       {
         motorAngle = 0;
-      }  
-      delay(5);  
+      }
+      delay(5);
     };
-  } 
+  }
 }
 
-void ultraSonic() {
-  digitalWrite(trigPin, LOW);// Clears the trigPin
+void ultraSonic()
+{
+  digitalWrite(trigPin, LOW); // Clears the trigPin
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);// Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
-  duration = pulseIn(echoPin, HIGH);// Reads the echoPin, returns the sound wave travel time in microseconds
-  
-  distance = duration * SOUND_VELOCITY/2;// Calculate the distance
+
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+
+  distance = duration * SOUND_VELOCITY / 2; // Calculate the distance
 }
 
-void printTemp(){
-  if (distance >= 11 && distance <= 17){
-  while (i < 30) {
-    temp= mlx.readObjectTempC();
-    sum += temp;
-    i++;
-    delay(1);
+void printTemp()
+{
+  if (distance >= 11 && distance <= 17)
+  {
+    while (i < 30)
+    {
+      temp = mlx.readObjectTempC();
+      sum += temp;
+      i++;
+      delay(1);
+    }
+    temperature = sum / 30.0;
+    sum = 0;
+    i = 0;
+    Serial.println(temperature);
+    delay(50);
   }
-  temperature = sum/30.0;
-  sum = 0;
-  i = 0;
-  Serial.println(temperature);
-  delay(50);
-  }
-  }
+}
 
-void loop() {
-ultraSonic();
-printTemp();
-controlMotor();
-delay(200);
+void loop()
+{
+  ultraSonic();
+  printTemp();
+  controlMotor();
+  delay(200);
 }
